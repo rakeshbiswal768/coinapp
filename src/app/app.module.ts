b73c4bc565f753f64a77a56a8dde008ddef39e83
Rakesh Biswal <rakesh@techlift.in>
2018-02-22 09:09:41 +0530

    HTML page integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import {Home} from './home/home';
+import {Login} from './login/login';
+import {Portfolio} from './portfolio/portfolio';
+import {Setting} from './setting/setting';
+import {Watchlist} from './watchlist/watchlist';
+import {Error403} from './error403/error403';
+import {Error404} from './error404/error404';
+import {Error405} from './error405/error405';
+import {Error500} from './error500/error500';
+import {Coindetails} from './coindetails/coindetails';
+import {Admin} from './admin/admin';
+import {Addnews} from './addnews/addnews';
+import {Addcoin} from './addcoin/addcoin';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Home,
+    Login,
+    Portfolio,
+    Setting,
+    Watchlist,
+    Error403,
+    Error404,
+    Error405,
+    Error500,
+    Coindetails,
+    Admin,
+    Addnews,
+    Addcoin
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [Login]
 })
 export class AppModule { }
